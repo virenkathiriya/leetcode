f7f4a3702e4fc89e75995572b42f2ad130e9b3ed
Viren Kathiriya <42746942+virenkathiriya@users.noreply.github.com>
2022-03-12 20:37:12 +0530

    Time: 43 ms (66.81%), Space: 12.2 MB (97.29%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
+    int n, m;
     void dfs(vector<vector<char>>& g, int x, int y) {
-        if(g[x][y] != '1') return;
-        int n = g.size(), m = g[0].size();        
+        if(g[x][y] != '1') return;      
         g[x][y] = '0';
         if(x - 1 >= 0) dfs(g, x - 1, y);
         if(x + 1 < n) dfs(g, x + 1, y);
@@ -10,7 +10,7 @@ public:
         if(y + 1 < m) dfs(g, x, y + 1);
     }
     int numIslands(vector<vector<char>>& g) {
-        int n = g.size(), m = g[0].size();
+        n = g.size(), m = g[0].size();
         int cnt = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
