24658f36a81766de83379b77ea19b93e0525c9a6
Viren Kathiriya <42746942+virenkathiriya@users.noreply.github.com>
2022-03-13 22:23:54 +0530

    Time: 16 ms (54.45%), Space: 17.7 MB (46.45%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -15,28 +15,50 @@ public:
             for (int i = 0; i < N; i++) ans += min(l[i], r[i]) - v[i];
             return ans;
         */
-        stack<int> st;
-        int N = v.size(), ans = 0;
-        for(int i = 0; i < N; i++) {
-            int x = v[i];
-            if (st.empty()) {
+        /*
+            stack<int> st;
+            int N = v.size(), ans = 0;
+            for(int i = 0; i < N; i++) {
+                int x = v[i];
+                if (st.empty()) {
+                    st.push(i);
+                    continue;
+                }
+
+                while (!st.empty() && v[st.top()] < x) {
+                    int top = st.top(); // bar on top of which water will trap
+                    st.pop();
+                    if(st.empty())
+                        break; // that means it doesn't have left bar to trap water
+
+                    // now st.top() will be either equal or larger than the bar we are trying to tap water into
+                    int dx = i - st.top() - 1;
+                    int dy = min(x, v[st.top()]) - v[top];
+
+                    ans += dx * dy;
+                }
                 st.push(i);
-                continue;
             }
-            
-            while (!st.empty() && v[st.top()] < x) {
-                int top = st.top(); // bar on top of which water will trap
-                st.pop();
-                if(st.empty())
-                    break; // that means it doesn't have left bar to trap water
-                
-                // now st.top() will be either equal or larger than the bar we are trying to tap water into
-                int dx = i - st.top() - 1;
-                int dy = min(x, v[st.top()]) - v[top];
-                
-                ans += dx * dy;
+            return ans;
+        */
+        /*
+            two pointer
+            : optimizing 1st approach
+            : maintaining lmax and rmax
+            : which ever side is lower will move to the other as in first we atke min of l, r
+        */
+        int lmax = 0, rmax = 0, ans = 0;
+        int l = 0, r = v.size() - 1;
+        while (l < r) {
+            if (v[l] < v[r]) {
+                if (v[l] > lmax) lmax = v[l];
+                else ans += lmax - v[l];
+                l++;
+            } else {
+                if (v[r] > rmax) rmax = v[r];
+                else ans += rmax - v[r];
+                r--;
             }
-            st.push(i);
         }
         return ans;
     }
