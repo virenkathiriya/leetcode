a9effe9abec9b2d37a1fb49f856f7884a225c5de
Viren Kathiriya <42746942+virenkathiriya@users.noreply.github.com>
2022-03-25 11:04:40 +0530

    Time: 20 ms (12.49%), Space: 14 MB (12.38%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -2,38 +2,41 @@ class Solution {
 public:
     int orangesRotting(vector<vector<int>>& g) {
         int n = g.size(), m = g[0].size();
-        vector<vector<int>> dist(n, vector<int> (m, INT_MAX - 1));
         queue<vector<int>> q;
-        for (int i = 0; i < n; i++) for (int j = 0; j < m; j++) if (g[i][j] == 2) {
-            dist[i][j] = 0;
-            q.push({i, j});
-        }
+        int fresh = 0;
+        for (int i = 0; i < n; i++) 
+            for (int j = 0; j < m; j++) 
+                if (g[i][j] == 2) {
+                    q.push({i, j});
+                } else if (g[i][j] == 1) fresh++;
         
         vector<vector<int>> dir = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        int ans = -1;
         while (!q.empty()) {
-            auto cur = q.front();
-            q.pop();
-            for (int i = 0; i < dir.size(); i++) {
-                int x = cur[0], y = cur[1];
-                int xN = x + dir[i][0], yN = y + dir[i][1];
-                if (xN < 0|| xN >= n || yN < 0 || yN >= m) continue;
-                if (g[xN][yN] == 1 && dist[xN][yN] > dist[x][y] + 1) {
-                    dist[xN][yN] = dist[x][y] + 1;
-                    q.push({xN, yN});
+            int s = q.size(); // rotten oranges at ans + 1 minute
+            
+            while(s--) {
+                auto cur = q.front();
+                q.pop();
+                for (int i = 0; i < dir.size(); i++) {
+                    int x = cur[0], y = cur[1];
+                    int xN = x + dir[i][0], yN = y + dir[i][1];
+                    if (xN < 0|| xN >= n || yN < 0 || yN >= m) continue;
+                    if (g[xN][yN] == 1) {
+                        g[xN][yN] = 2;
+                        fresh--;
+                        q.push({xN, yN});
+                    }
                 }
             }
+            ans++;
         }
         
-        int ans = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0;j < m; j++) {
-                if (g[i][j] == 1 && dist[i][j] == INT_MAX - 1) {
-                    return -1;
-                } else if (g[i][j] == 1){
-                    ans = max(ans, dist[i][j]);
-                }
-            }
-        }
+        if (fresh) 
+            return -1;
+        
+        if (ans == -1) // there are no rotten or fresh oranges 
+            return 0;
         return ans;
     }
 };
