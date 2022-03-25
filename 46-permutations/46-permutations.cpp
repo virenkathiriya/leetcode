a45bacf78d39e1b87257a0cec8b178f4a4e5bb9b
Viren Kathiriya <42746942+virenkathiriya@users.noreply.github.com>
2022-03-25 15:24:33 +0530

    Time: 7 ms (33.70%), Space: 8.1 MB (21.35%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -3,24 +3,19 @@ public:
     vector<vector<int>> permute(vector<int>& v) {
         vector<vector<int>> ans;
         int n = v.size();
-        vector<bool> vis(n, false);
-        f(n, v, {}, vis, 0, ans);
+        f(v, ans, 0, n);
         return ans;
     }
     
-    void f(int &n, vector<int> &v, vector<int> cur_p, vector<bool> vis, int index, vector<vector<int>> &ans) {
-        if (index == n) {
-            ans.push_back(cur_p);
+    void f(vector<int> v, vector<vector<int>> &ans, int l, int r) {
+        if (l == r) {
+            ans.push_back(v);
             return;
         }
-        for (int i = 0; i < n; i++) {
-            if (!vis[i]) {
-                vector<int> cur_p_with_i_included(cur_p.begin(), cur_p.end());
-                cur_p_with_i_included.push_back(v[i]);
-                vector<bool> cur_vis_with_i_included(vis.begin(), vis.end());
-                cur_vis_with_i_included[i] = true;
-                f(n, v, cur_p_with_i_included, cur_vis_with_i_included, index + 1, ans);
-            }
+        for (int i = l; i < r; i++) {
+            swap(v[i], v[l]);
+            f(v, ans, l + 1, r);
+            swap(v[i], v[l]);
         }
     }
 };
