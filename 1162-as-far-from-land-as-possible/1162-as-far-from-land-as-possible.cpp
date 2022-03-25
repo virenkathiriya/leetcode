9faf632524974e2aa883973a44d52fffd7fe2504
Viren Kathiriya <42746942+virenkathiriya@users.noreply.github.com>
2022-03-25 21:03:23 +0530

    Time: 60 ms (88.08%), Space: 18.4 MB (93.42%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -3,35 +3,33 @@ public:
     int maxDistance(vector<vector<int>>& g) {
         int n = g.size(), m = g[0].size();
         int mx = m + n;
-        queue<pair<int, int>> q;
         vector<vector<int>> dp(n, vector<int> (m, mx));
-        
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (g[i][j] == 1) {
-                    q.push({i, j});
                     dp[i][j] = 0;
                 }
-            }
-        }
-        
-        vector<vector<int>> d = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-        if (q.empty() || q.size() == n * m) return -1;
-        int ans = 0;
-        while (!q.empty()) {
-            auto it = q.front();
-            q.pop();
-            int x = it.first, y = it.second;
-            for (int i = 0; i < 4; i++) {
-                int xN = d[i][0] + x, yN = d[i][1] + y;
-                if (xN < 0 || xN >= n || yN < 0 || yN >= m) continue;
-                if (dp[xN][yN] > dp[x][y] + 1) {
-                    ans = max(ans, dp[xN][yN] = dp[x][y] + 1);
-                    q.push({xN, yN});
+                if (i - 1 >= 0 && dp[i - 1][j] != mx) {
+                    dp[i][j] = min(dp[i][j], dp[i - 1][j] + 1);
+                }
+                if (j - 1 >= 0 && dp[i][j - 1] != mx) {
+                    dp[i][j] = min(dp[i][j], dp[i][j - 1] + 1);
                 }
             }
         }
+        int ans = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+                if (i + 1 < n && dp[i + 1][j] != mx) {
+                    dp[i][j] = min(dp[i][j], dp[i + 1][j] + 1);
+                }
+                if (j + 1 < m && dp[i][j + 1] != mx) {
+                    dp[i][j] = min(dp[i][j], dp[i][j + 1] + 1);
+                }
+                ans = max(ans, dp[i][j]);
+            }
+        }
         
-        return ans;
+        return ((ans == 0 || ans == m + n) ? -1 : ans);
     }
 };
