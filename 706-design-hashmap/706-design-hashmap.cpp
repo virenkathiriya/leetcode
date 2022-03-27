28909f5a1a715281a5890e760553192df9573abf
Viren Kathiriya <42746942+virenkathiriya@users.noreply.github.com>
2022-03-27 20:12:20 +0530

    Time: 225 ms (43.14%), Space: 55.2 MB (62.60%) - LeetHub

--- 706-design-hashmap/706-design-hashmap.cpp
+++ 706-design-hashmap/706-design-hashmap.cpp
@@ -1,32 +1,33 @@
 class MyHashMap {
 public:
-    vector<vector<int>> store[1000];
+    static const int size = 950;
+    vector<vector<int>> store[size];
     MyHashMap() {
     }
     
     void put(int key, int value) {
-        for (auto &it: store[key % 1000]) {
+        for (auto &it: store[key % size]) {
             if (it[0] == key) {
                 it[1] = value;
                 return;
             };
         }
-        store[key % 1000].push_back({key, value});
+        store[key % size].push_back({key, value});
     }
     
     int get(int key) {
-        for (auto &it: store[key % 1000]) {
+        for (auto &it: store[key % size]) {
             if (it[0] == key) return it[1];
         } 
         return -1;
     }
     
     void remove(int key) {
-        int N = store[key % 1000].size();
+        int N = store[key % size].size();
         for (int i = 0; i < N; i++) {
-            if (store[key % 1000][i][0] == key) {
-                if (i != N - 1) swap(store[key % 1000][i], store[key % 1000][N - 1]);
-                store[key % 1000].pop_back();
+            if (store[key % size][i][0] == key) {
+                if (i != N - 1) swap(store[key % size][i], store[key % size][N - 1]);
+                store[key % size].pop_back();
                 break;
             }
         }
