829a5a0fe28fdade987e6aea999af341e9ff4fa6
Viren Kathiriya <42746942+virenkathiriya@users.noreply.github.com>
2022-03-27 19:00:42 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (24.55%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -3,11 +3,7 @@ public:
     void sortColors(vector<int>& v) {
         int n = v.size();
         int l = 0, r = n - 1;
-        for (int i = l; i <= r;) {
-            while (l < n && v[l] == 0) l++;
-            while (l < r && v[r] == 2) r--;
-            if (i < l) i = l;
-            if (i >= n || !(l <= r)) break;
+        for (int i = l; i <= r && i < n;) {
             if (v[i] == 0) {
                 swap (v[l], v[i]);
                 l++;
@@ -17,6 +13,8 @@ public:
             } else {
                 i++;
             }
+            
+            if (i < l) i = l;
         }
     }
 };
