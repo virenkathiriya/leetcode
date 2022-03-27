35a747778f7b79ddd8d66c22140980571453c7f1
Viren Kathiriya <42746942+virenkathiriya@users.noreply.github.com>
2022-03-27 23:31:06 +0530

    Time: 2 ms (43.80%), Space: 6.2 MB (56.08%) - LeetHub

---
+++ 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool checkString(string s) {
+        bool ok = true;
+        for (char &c: s) {
+            if (c == 'b') ok = false;
+            else if (!ok && c == 'a') return false;
+        }
+        return true;
+    }
+};
