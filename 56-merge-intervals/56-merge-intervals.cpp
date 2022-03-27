a1fa3726d147b36ca80b30bbab621736ef15c607
Viren Kathiriya <42746942+virenkathiriya@users.noreply.github.com>
2022-03-27 19:16:04 +0530

    Time: 63 ms (33.99%), Space: 20.1 MB (26.14%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,37 +1,14 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& v) {
-        if (v.size() == 1) return v;
-        int N = v.size(), MX = 10002;
-        
-        vector<int> p(MX, 0), single(MX, 0);
-        vector<vector<int>> ans; 
-        for (int i = 0; i < N; i++) {
-            if (v[i][0] == v[i][1]) 
-                single[v[i][0]] = 1;
-            else {
-                p[v[i][0]]++; 
-                p[v[i][1]]--;
-            }
-        }
-        for (int i = 1; i < MX; i++) p[i] += p[i - 1];
-        
-        int l = -1, r = -1;
-        for (int i = 0; i < MX; i++) {
-            bool ending = false;
-            if (p[i] == 0) {
-                if (l != -1) {
-                    ans.push_back({l, i});
-                    ending = true;
-                    l = r = -1;
-                }
-            } else if (l == -1) {
-                l = i;
-            }
-            if (single[i]) {
-                if (l == -1 && !ending) 
-                    ans.push_back({i, i});
-            }  
+        vector<vector<int>> ans;
+        sort(v.begin(), v.end());
+        for (auto it: v) {
+            if (ans.empty() || ans.back()[1] < it[0]) {
+                ans.push_back(it);
+                continue;
+            } 
+            ans.back()[1] = max(ans.back()[1], it[1]);
         }
         return ans;
     }
