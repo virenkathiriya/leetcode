bfe903a5f4678de7199f4fa6335bf95c11186f48
Viren Kathiriya <42746942+virenkathiriya@users.noreply.github.com>
2022-03-28 15:04:53 +0530

    Time: 14 ms (42.53%), Space: 13.8 MB (28.81%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     int deleteAndEarn(vector<int>& v) {
-        unordered_map<int, int> mp;
-        int mx = 10001;
+        vector<int> mp(10001, 0);
         for (int &x: v) mp[x]++;
         int i1 = mp[1], i2 = 0;
-        for (int i = 2; i < mx; i++) {
+        for (int i = 2; i < 10001; i++) {
             swap(i1, i2);
             i1 = max(i1 + mp[i] * i, i2);
         }
