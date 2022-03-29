b21e7891bd51ca9076168613a1fd414cabc9d073
Viren Kathiriya <42746942+virenkathiriya@users.noreply.github.com>
2022-03-29 18:32:06 +0530

    Time: 21 ms (72.33%), Space: 16.5 MB (97.07%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -2,13 +2,18 @@ class Solution {
 public:
     int jump(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n, 10001);
-        dp[n - 1] = 0;
-        for (int i = n - 2; i >= 0; i--) {
-            for (int j = 1; j <= nums[i] && i + j < n; j++) {
-                dp[i] = min(dp[i], dp[i + j] + 1);
+        int currentRightBound = 0;
+        int ans = 0;
+        int nextRightBound = 0;
+        int i = 0;
+        while (currentRightBound < n - 1) {
+            nextRightBound = max(nextRightBound, i + nums[i]);
+            if (i == currentRightBound) {
+                currentRightBound = nextRightBound;
+                ans++;
             }
+            i++;
         }
-        return dp[0];
+        return ans;
     }
 };
