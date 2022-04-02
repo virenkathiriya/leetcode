a00ba3d8649c9e5f53ce063a92fcc1496b24e231
Viren Kathiriya <42746942+virenkathiriya@users.noreply.github.com>
2022-04-02 11:43:07 +0530

    Time: 34 ms (32.09%), Space: 12 MB (54.34%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
@@ -2,19 +2,18 @@ class Solution {
 public:
     vector<vector<int>> ans;
     int n;
-    void dfs(int u, vector<vector<int>>& g, vector<int> cur) {
+    void dfs(int u, vector<vector<int>>& g, vector<int> &cur) {
         cur.push_back(u);
-        if (u == n) {
+        if (u == n) 
             ans.push_back(cur);
-            return;
-        }
-        for (int &x: g[u]) {
-            dfs(x, g, cur);
-        }
+        else
+            for (int &x: g[u]) dfs(x, g, cur);
+        cur.pop_back();
     }
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& g) {
         n = g.size() - 1;
-        dfs(0, g, {});
+        vector<int> cur;
+        dfs(0, g, cur);
         return ans;
     }
 };
