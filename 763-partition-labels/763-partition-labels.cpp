1ef3acf11c83c06b909d96e3af4932dd64f1ca09
Viren Kathiriya <42746942+virenkathiriya@users.noreply.github.com>
2022-04-02 12:46:22 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (95.86%) - LeetHub

--- 763-partition-labels/763-partition-labels.cpp
+++ 763-partition-labels/763-partition-labels.cpp
@@ -2,25 +2,17 @@ class Solution {
 public:
     vector<int> partitionLabels(string s) {
         int n = s.length();
-        vector<int> f(26, 0);
-        for (char &c: s) f[c - 'a']++;
+        vector<int> l(26, 0);
+        for (int i = 0; i < n; i++) l[s[i] - 'a'] = i;
         
-        vector<int> cur(26, 0);
         vector<int> ans;
-        int cnt = 0;
-        for (char &c: s) {
-            int x = c - 'a';
-            cnt++;
-            cur[x]++;
-            bool ok = true;
-            for (int i = 0; i < 26; i++) {
-                if (cur[i] == 0) continue;
-                ok &= cur[i] == f[i];
-                if (!ok) break;
-            }
-            if (ok) {
-                ans.push_back(cnt);
-                cnt = 0;
+        int prev = 0;
+        int r = 0;
+        for (int i = 0; i < n; i++) {
+            r = max(r, l[s[i] - 'a']);
+            if (i == r) {
+                ans.push_back(i - prev + 1);
+                prev = i + 1;
             }
         }
         return ans;
