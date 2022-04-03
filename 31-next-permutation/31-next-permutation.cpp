a136906e2537b364325cfd429031676d2831d159
Viren Kathiriya <42746942+virenkathiriya@users.noreply.github.com>
2022-04-03 10:01:59 +0530

    Time: 3 ms (87.24%), Space: 12.1 MB (75.54%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,20 +1,31 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& v) {
-        if(v.size() == 1) return;
-        int N = v.size();
-        int i = N - 2, j = N - 1;
-        // finding the break point
-        while(i >= 0 && v[i] >= v[i + 1]) i--;
-        if(i >= 0) {
-            // next value that will replace the breakpoint
-            while(j >= i && v[i] >= v[j]) j--;
-            swap(v[i], v[j]);
-        } else {
+        int n = v.size();
+        if (n == 1) return;
+        // finding breakpoint
+        int b = -1;
+        for (int i = n - 2; i >= 0; i--) {
+            if (v[i] < v[i + 1]) {
+                b = i;
+                break;
+            }
+        }
+        if (b == -1) {
             reverse(v.begin(), v.end());
             return;
         }
-        if (i + 1 < N)
-            reverse(v.begin() + i + 1, v.end());
+        
+        // finding next larger element 
+        int l = -1;
+        for (int i = n - 1; i > b; i--) {
+            if (v[i] > v[b]) {
+                l = i;
+                break;
+            }
+        }
+        
+        swap(v[l], v[b]);
+        sort(v.begin() + b + 1, v.end());
     }
 };
