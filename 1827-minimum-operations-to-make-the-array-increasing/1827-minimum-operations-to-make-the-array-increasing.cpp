c813a464866c4ebf1bdd3c13fd7737dd2d4eee28
Viren Kathiriya <42746942+virenkathiriya@users.noreply.github.com>
2022-04-06 11:53:25 +0530

    Time: 22 ms (45.89%), Space: 15.8 MB (30.87%) - LeetHub

--- 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.cpp
+++ 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.cpp
@@ -2,7 +2,11 @@ class Solution {
 public:
     int minOperations(vector<int>& v) {
         int ans = 0;
-        for (int i = 1; i < v.size(); i++) {ans += max(0, v[i - 1] + 1 - v[i]); v[i] = max(v[i], v[i - 1] + 1);}
+        for (int i = 1; i < v.size(); i++) {
+            if (v[i] > v[i - 1]) continue;
+            ans += v[i - 1] + 1 - v[i]; 
+            v[i] = v[i - 1] + 1;
+        }
         return ans;
     }
 };
