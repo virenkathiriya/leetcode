247f2e6c585742c3e916c9c487a068393dce9832
Viren Kathiriya <42746942+virenkathiriya@users.noreply.github.com>
2022-04-06 10:24:00 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (81.62%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.cpp
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.cpp
@@ -5,7 +5,7 @@ public:
         int n = s.size();
         for (int i = 0; i < n; i++) {
             if (s[i] == 'G') ans += s[i];
-            else if (i + 1 < n && s[i] == '(' && s[i + 1] == ')') {ans += 'o'; i++;}
+            else if (s[i + 1] == ')') {ans += 'o'; i++;}
             else {ans += "al"; i += 3;}
         }
         return ans;
