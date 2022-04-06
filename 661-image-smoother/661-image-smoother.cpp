888c4e0e68d677be6c6d9bce8193417152796f89
Viren Kathiriya <42746942+virenkathiriya@users.noreply.github.com>
2022-04-06 15:54:21 +0530

    Time: 79 ms (48.65%), Space: 22 MB (24.84%) - LeetHub

---
+++ 661-image-smoother/661-image-smoother.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> imageSmoother(vector<vector<int>>& g) {
+        int n = g.size(), m = g[0].size();
+        vector<vector<int>> ans(n, vector<int> (m, 0));
+        vector<vector<int>> di = {{0, 1}, {0, 0}, {0, -1}, {-1, -1}, {-1, 0}, {-1, 1}, {1, -1}, {1, 0}, {1, 1}};
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                int cnt = 0, sum = 0;
+                for (vector<int> &d: di) {
+                    int X = i + d[0], Y = j + d[1];
+                    if (X < 0 || Y < 0 || X >= n || Y >= m) continue;
+                    cnt++;
+                    sum += g[X][Y];
+                }
+                ans[i][j] = sum / cnt;
+            }
+        }
+        return ans;
+    }
+};
