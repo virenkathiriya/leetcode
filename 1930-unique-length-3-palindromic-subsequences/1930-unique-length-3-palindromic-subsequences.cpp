bc93dfa0763a8d8d526172b4f6a3f70c25a3802e
Viren Kathiriya <42746942+virenkathiriya@users.noreply.github.com>
2022-04-07 00:35:23 +0530

    Time: 1325 ms (10.95%), Space: 13.3 MB (53.00%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
@@ -2,14 +2,6 @@ class Solution {
 public:
     int countPalindromicSubsequence(string s) {
         int n = s.size();
-        vector<vector<int>> v(n, vector<int> (26, 0));
-        for (int i = 0; i < n; i++) {
-            if (i) {
-                for (int j = 0; j < 26; j++) v[i][j] = v[i - 1][j];
-            }
-            v[i][s[i] - 'a']++;
-        }
-        
         vector<int> f(26, -1), l(26, -1);
         for (int i = 0; i < n; i++) {
             if (f[s[i] - 'a'] == -1) f[s[i] - 'a'] = i;
@@ -18,9 +10,9 @@ public:
         int ans = 0;
         for (int i = 0; i < 26; i++) {
             if (l[i] > f[i]) {
-                for (int k = 0; k < 26; k++) {
-                    ans += (v[l[i] - 1][k] - v[f[i]][k] > 0);
-                }
+                set<int> st;
+                for (int j = f[i] + 1; j < l[i]; j++) st.insert(s[j]);
+                ans += st.size();
             }
         }
         return ans;
