996fbc3d070922492af7b2f39672ab95f8c1a763
Viren Kathiriya <42746942+virenkathiriya@users.noreply.github.com>
2022-04-06 10:29:40 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (98.48%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        vector<int> f(26, 0), f1(26, 0);
-        for (char &c: s) f[c - 'a']++;
-        for (char &c: t) f1[c - 'a']++;
-        for (int i = 0; i < 26; i++) if (f[i] != f1[i]) return i + 'a';
-        return ' ';
+        return accumulate(t.begin(), t.end(), 0) - accumulate(s.begin(), s.end(), 0);;
     }
 };
