7ac9ee81b00f810f00d9a0e5da263e3db91409a0
Viren Kathiriya <42746942+virenkathiriya@users.noreply.github.com>
2022-04-07 13:22:25 +0530

    Time: 57 ms (50.87%), Space: 6.1 MB (83.11%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
-    int sum(vector<long long> c) {
-        long long ans = 0;
-        int MOD = 1000000007;
-        for (auto &x: c) {
-            ans = (ans + x) % MOD;
-        }
-        return ans;
-    }
     int countVowelPermutation(int n) {
         n++;
-        vector<vector<long long>> dp(n, vector<long long> (5, 0));
-        for (int i = 0; i < 5; i++) dp[1][i] = 1;
+        vector<vector<long long>> dp(2, vector<long long> (5, 0));
+        for (int i = 0; i < 5; i++) dp[0][i] = 1;
+        int MOD = 1000000007;
         
         for (int i = 2; i < n; i++) {
-            dp[i][0] = dp[i - 1][1];
-            dp[i][1] = sum ({dp[i - 1][0], dp[i - 1][2]});
-            dp[i][2] = sum ({dp[i - 1][0], dp[i - 1][1], dp[i - 1][3], dp[i - 1][4]});
-            dp[i][3] = sum ({dp[i - 1][2], dp[i - 1][4]});
-            dp[i][4] = dp[i - 1][0];
+            dp[1][0] = dp[0][1];
+            dp[1][1] = (dp[0][0] + dp[0][2]) % MOD ;
+            dp[1][2] = (dp[0][0] + dp[0][1] + dp[0][3] + dp[0][4]) % MOD;
+            dp[1][3] = (dp[0][2] + dp[0][4]) % MOD;
+            dp[1][4] = dp[0][0];
+            for (int j = 0; j < 5; j++) swap(dp[1][j], dp[0][j]);
         }
-        return sum(dp[n - 1]);
+        return (dp[0][0] + dp[0][1] + dp[0][2] + dp[0][3] + dp[0][4]) % MOD;
     }
 };
