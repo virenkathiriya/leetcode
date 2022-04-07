15f60eb5c93694961e17437d8841f46d5a2a7ca1
Viren Kathiriya <42746942+virenkathiriya@users.noreply.github.com>
2022-04-07 21:21:36 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (41.36%) - LeetHub

--- 2027-minimum-moves-to-convert-string/2027-minimum-moves-to-convert-string.cpp
+++ 2027-minimum-moves-to-convert-string/2027-minimum-moves-to-convert-string.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int minimumMoves(string s) {
-        int c = 0;
-        for (int i = 0; i < s.length(); i++) {
+        int c = 0, n = s.size();
+        for (int i = 0; i < n; i++) {
             if (s[i] == 'X') {
                 c++;
                 i += 2;
