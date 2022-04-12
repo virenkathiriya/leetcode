d2d9b5f157d546e85660d295b24ea66dbbafc3f2
Viren Kathiriya <42746942+virenkathiriya@users.noreply.github.com>
2022-04-12 11:08:51 +0530

    Time: 4 ms (89.83%), Space: 7.8 MB (85.23%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -3,10 +3,10 @@ public:
     bool wordBreak(string s, vector<string>& wd) {
         int n = s.size();
         vector<bool> dp(n + 1, false);
-        priority_queue<int> q;
+        queue<int> q;
         q.push(0);
         while (!q.empty()) {
-            int ind = q.top();
+            int ind = q.front();
             if (ind == n) return true;
             q.pop();
             for (string &w: wd) {
