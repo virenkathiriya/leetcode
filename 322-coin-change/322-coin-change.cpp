7904d6eb96f51b2d11fdc1733aa89f37e8aaae2b
Viren Kathiriya <42746942+virenkathiriya@users.noreply.github.com>
2022-04-13 08:08:20 +0530

    Time: 222 ms (14.24%), Space: 22.6 MB (23.05%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -5,19 +5,20 @@ public:
         
         sort(c.begin(), c.end());
         int n = c.size();
-        vector<vector<int>> dp(n, vector<int> (A + 1, INT_MAX));
+        vector<vector<int>> dp(2, vector<int> (A + 1, INT_MAX));
         if (c[0] > A) return -1;
         for (int i = c[0]; i <= A; i += c[0])
             dp[0][i] = i / c[0];
-        for (int i = 0; i < n; i++) dp[i][0] = 0;
+        for (int i = 0; i < 2; i++) dp[i][0] = 0;
     
         for (int i = 1; i < n; i++) {
+            int t = i % 2;
             for (int j = 1; j <= A; j++) {
-                dp[i][j] = dp[i - 1][j];
-                if (j >= c[i] && dp[i][j - c[i]] != INT_MAX)
-                    dp[i][j] = min(dp[i][j - c[i]] + 1, dp[i][j]);
+                dp[t][j] = dp[!t][j];
+                if (j >= c[i] && dp[t][j - c[i]] != INT_MAX)
+                    dp[t][j] = min(dp[t][j - c[i]] + 1, dp[t][j]);
             }
         }
-        return dp[n - 1][A] == INT_MAX ? -1: dp[n - 1][A];
+        return dp[(n + 1) % 2][A] == INT_MAX ? -1: dp[(n + 1) % 2][A];
     }
 };
