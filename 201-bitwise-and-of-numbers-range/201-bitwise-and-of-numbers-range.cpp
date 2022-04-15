3c036867dbdc6150ebfea285970ed0c87e314970
Viren Kathiriya <42746942+virenkathiriya@users.noreply.github.com>
2022-04-15 22:42:55 +0530

    Time: 16 ms (50.26%), Space: 5.9 MB (32.67%) - LeetHub

---
+++ 201-bitwise-and-of-numbers-range/201-bitwise-and-of-numbers-range.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int rangeBitwiseAnd(int l, int r) {
+        int c = 0;
+        while (l != r) {
+            l >>= 1;
+            r >>= 1;
+            c++;
+        }
+        return l << c;
+    }
+};
