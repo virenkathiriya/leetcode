c1ebd52f65083e5810027e3b64664859c828e24a
Viren Kathiriya <42746942+virenkathiriya@users.noreply.github.com>
2022-04-23 18:22:47 +0530

    Time: 25 ms (25.47%), Space: 10.5 MB (9.84%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.cpp
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.cpp
@@ -4,19 +4,7 @@ public:
         int n = g.size(), m = g[0].size();
         int i = 0, j =  m - 1;
         int ans = 0;
-        while (i < n && j >= 0) {
-            if (g[i][j] >= 0) {
-                i++;
-                continue;
-            }
-            if (j > 0 && g[i][j - 1] < 0) {
-                ans += (n - i);
-                j--;
-            } else {
-                ans++;
-                i++;
-            }
-        }
+        while (i < n && j >= 0) (g[i][j] >= 0) ? i++ : (j > 0 && g[i][j - 1] < 0) ? (ans += (n - i), j--): (ans++, i++);
         return ans;
     }
 };
