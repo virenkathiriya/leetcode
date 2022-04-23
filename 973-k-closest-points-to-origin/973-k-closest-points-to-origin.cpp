61a1fa1fabcd840ac6342ce627436e0c1cd6fd48
Viren Kathiriya <42746942+virenkathiriya@users.noreply.github.com>
2022-04-23 17:57:06 +0530

    Time: 337 ms (47.24%), Space: 57.1 MB (85.60%) - LeetHub

---
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& p, int k) {
+        sort(begin(p), end(p), [&](const vector<int> &a, const vector<int> &b) {
+            return abs(a[0] * a[0] + a[1] * a[1]) < abs(b[0] * b[0] + b[1] * b[1]);
+        });
+        p.resize(k);
+        return p;
+    }
+};
