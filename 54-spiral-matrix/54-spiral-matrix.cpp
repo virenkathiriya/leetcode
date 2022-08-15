75ecda0e793c7f545e35a536f725b7294c0c432d
Viren Kathiriya <42746942+virenkathiriya@users.noreply.github.com>
2022-08-15 13:24:07 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (92.11%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.cpp
+++ 54-spiral-matrix/54-spiral-matrix.cpp
@@ -1,28 +1,38 @@
 class Solution {
 public:
-    vector<int> spiralOrder(vector<vector<int>>& g) {
+    vector<int> spiralOrder(vector<vector<int>>& v) {
+        int d = 1, cnt = 0;
         vector<int> ans;
-        int n = g.size(), m = g[0].size();
-        int i = 0, j = 0, cnt = 0, d = 0;
-        while (cnt < m * n) {
-            if (g[i][j] != 101) {
-                ans.push_back(g[i][j]);
-                g[i][j] = 101;
+        int i = 0, j = 0;
+        int n = v.size(), m = v[0].size();
+        int set = -101;
+        while (cnt < n * m) {
+            if (v[i][j] != set) {
+                ans.push_back(v[i][j]);
                 cnt++;
+                v[i][j] = set;
             }
             switch (d) {
-                case 0:
-                    (j + 1 < m && g[i][j + 1] != 101) ? j++: d++;
+                case 1: {
+                    // right
+                    (j + 1 < m && v[i][j + 1] != set) ? j++ : d++;
                     break;
-                case 1:
-                    (i + 1 < n && g[i + 1][j] != 101) ? i++: d++;
+                }
+                case 2: {
+                    // down
+                    (i + 1 < n && v[i + 1][j] != set) ? i++ : d++;
                     break;
-                case 2:
-                    (j - 1 >= 0 && g[i][j - 1] != 101) ? j-- : d++;
+                }
+                case 3: {
+                    // left
+                    (i - 1 >= 0 && v[i - 1][j] != set) ? i-- : d++;
                     break;
-                case 3:
-                    (i - 1 >= 0 && g[i - 1][j] != 101) ? i-- : d++;
+                }
+                case 0: {
+                    // up
+                    (j - 1 >= 0 && v[i][j - 1] != set) ? j-- : d++;
                     break;
+                }
             }
             d %= 4;
         }
