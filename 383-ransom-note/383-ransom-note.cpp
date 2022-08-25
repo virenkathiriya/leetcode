7bce260240bef8d4d6512de6d24632c549a13089
Viren Kathiriya <42746942+virenkathiriya@users.noreply.github.com>
2022-08-25 12:20:04 +0530

    Time: 10 ms (95.81%), Space: 8.8 MB (39.49%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     bool canConstruct(string a, string b) {
-        vector<int> f1(26, 0), f2(26, 0);
-        for (char &c: a) f1[c - 'a']++;
-        for (char &c: b) f2[c - 'a']++;
-        bool ok = true;
-        for (int i = 0; i < 26; i++) {
-            if (f1[i] > 0) ok &= f1[i] <= f2[i];
+        vector<int> f(26, 0);
+        for (char &c: b) f[c - 'a']++;
+        for (char &c: a) {
+            f[c - 'a']--;
+            if (f[c - 'a'] < 0) return false;
         }
-        return ok;
+        return true;
     }
 };
