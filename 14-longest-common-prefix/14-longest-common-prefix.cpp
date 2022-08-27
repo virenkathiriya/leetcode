60c173efe2f4010f9ce8fe4f58bb00deef3dcec0
Viren Kathiriya <42746942+virenkathiriya@users.noreply.github.com>
2022-08-27 12:46:58 +0530

    Time: 6 ms (62.51%), Space: 9.3 MB (56.06%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.cpp
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -1,44 +1,22 @@
 class Solution {
-private:
-    struct TrieNode {
-        char key;
-        vector<TrieNode*> alpha;
-        bool isEndOfString;
-        int size = 0;
-        TrieNode(char c) : key(c){
-            alpha.resize(26, NULL);
-            isEndOfString = false;
-        }
-    };
 public:
-    string longestCommonPrefix(vector<string>& st) {
-        TrieNode *root = new TrieNode(' ');
-        for (string s: st) {
-            int n = s.size();
-            if (n == 0) return "";
-            TrieNode *cur = root;
-            for (int i = 0 ; i < n; i++) {
-                int x = s[i] - 'a';
-                if (!cur -> alpha[x]) {
-                    cur -> alpha[x] = new TrieNode(s[i]);
-                    cur -> size = cur -> size + 1;
-                    if (cur -> size > 1) break;
-                } 
-                cur = cur -> alpha[x];
-                if (i == n - 1) cur -> isEndOfString = true;
-            }
-        }
-        if (root -> size > 1) return "";
-        TrieNode *ptr = root -> alpha[st[0][0] - 'a'];
+    string longestCommonPrefix(vector<string>& v) {
+        // ok i know tries but who cares
         string ans;
-        
-        while (ptr) {
-            ans += ptr -> key;
-            if (ptr -> size > 1 || ptr -> isEndOfString) return ans;
-            for (int i = 0; i < 26; i++) {
-                if (ptr -> alpha[i]) {
-                    ptr = ptr -> alpha[i];
-                    break;
+        for (int i = 0; i < 200; i++) {
+            for (int j = 0; j < v.size(); j++) {
+                if (j == 0) {
+                    // if current length is greater than defaulting first word.
+                    if (i >= v[j].size()) return ans;
+                    
+                    // set default 
+                    ans += v[j][i];
+                } else {
+                    // check current char with default 
+                    if (i >= v[j].size() || v[j][i] != ans[i]) {
+                        ans.pop_back(); 
+                        return ans;
+                    }
                 }
             }
         }
