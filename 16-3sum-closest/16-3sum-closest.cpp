c20474ac4fe9eec397b18ec3b89a625b8735cb9f
Viren Kathiriya <42746942+virenkathiriya@users.noreply.github.com>
2022-09-07 12:03:21 +0530

    Time: 1137 ms (7.58%), Space: 16.4 MB (57.69%) - LeetHub

---
+++ 16-3sum-closest/16-3sum-closest.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int threeSumClosest(vector<int>& v, int t) {
+        sort(v.begin(), v.end());
+        int N = v.size();
+        int ans = 1e8;
+        for (int i = 0; i < N; i++) {
+            int a = v[i];
+            int l = i + 1, r = N - 1;
+            while (l < r && l < N && r >= 0) {
+                int b = v[l], c = v[r];
+                if (abs(t - a - b - c) < abs(t - ans)) ans = a + b + c;
+                
+                int l1Diff = INT_MIN, r1Diff = INT_MIN;
+                if (l + 1 < N && l + 1 < r) {
+                    b = v[l + 1], c = v[r];
+                    l1Diff = abs(t - a - b - c);
+                } 
+                if (r - 1 >= 0 && r - 1 > l) {
+                    b = v[l], c = v[r - 1];
+                    r1Diff = abs(t - a - b - c);
+                }
+                // you are getting lesser diff (l + 1 or r - 1) move there
+                if (l1Diff < r1Diff) l++;
+                else r--;
+            }
+        }
+        return ans;
+    }
+};
+ 
