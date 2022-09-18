5eb177071e9055dba6123daa431a52170beb5e35
Viren Kathiriya <42746942+virenkathiriya@users.noreply.github.com>
2022-09-18 10:56:46 +0530

    Time: 19 ms (81.03%), Space: 19.7 MB (81.79%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,62 +1,17 @@
 class Solution {
 public:
     int trap(vector<int>& v) {
-        /*  
-            maintaining 2 arrays of lmax and rmax
-            int N = v.size();
-            int ans = 0;
-
-            vector<int> l(N, 0), r(N, 0);
-            l[0] = v[0];
-            for (int i = 1; i < N; i++) l[i] = max(l[i - 1], v[i]);
-            r[N - 1] = v[N - 1];
-            for (int i = N - 2; i >= 0; i--) r[i] = max(r[i + 1], v[i]);
-
-            for (int i = 0; i < N; i++) ans += min(l[i], r[i]) - v[i];
-            return ans;
-        */
-        /*
-            stack<int> st;
-            int N = v.size(), ans = 0;
-            for(int i = 0; i < N; i++) {
-                int x = v[i];
-                if (st.empty()) {
-                    st.push(i);
-                    continue;
-                }
-
-                while (!st.empty() && v[st.top()] < x) {
-                    int top = st.top(); // bar on top of which water will trap
-                    st.pop();
-                    if(st.empty())
-                        break; // that means it doesn't have left bar to trap water
-
-                    // now st.top() will be either equal or larger than the bar we are trying to tap water into
-                    int dx = i - st.top() - 1;
-                    int dy = min(x, v[st.top()]) - v[top];
-
-                    ans += dx * dy;
-                }
-                st.push(i);
-            }
-            return ans;
-        */
-        /*
-            two pointer
-            : optimizing 1st approach
-            : maintaining lmax and rmax
-            : which ever side is lower will move to the other as in first we atke min of l, r
-        */
-        int lmax = 0, rmax = 0, ans = 0;
-        int l = 0, r = v.size() - 1;
+        int n = v.size();
+        int l = 0, r = n - 1;
+        int lb = 0, rb = 0, ans = 0;
         while (l < r) {
             if (v[l] < v[r]) {
-                if (v[l] > lmax) lmax = v[l];
-                else ans += lmax - v[l];
+                if (lb >= v[l]) ans += lb - v[l];
+                else lb = v[l];
                 l++;
             } else {
-                if (v[r] > rmax) rmax = v[r];
-                else ans += rmax - v[r];
+                if (rb >= v[r]) ans += rb - v[r];
+                else rb = v[r];
                 r--;
             }
         }
