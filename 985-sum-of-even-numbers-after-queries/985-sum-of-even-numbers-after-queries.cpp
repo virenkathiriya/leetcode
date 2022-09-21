680eb1476902e78b0425515084e68d42f7655405
Viren Kathiriya <42746942+virenkathiriya@users.noreply.github.com>
2022-09-21 07:01:45 +0530

    Time: 252 ms (21.43%), Space: 45.9 MB (84.92%) - LeetHub

--- 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.cpp
+++ 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
-    vector<int> sumEvenAfterQueries(vector<int>& v, vector<vector<int>>& qu) {
-        vector<int> ans;
+    vector<int> sumEvenAfterQueries(vector<int>& v, vector<vector<int>>& q) {
         int sum = 0;
-        for (int &x: v) {
-            if (x % 2 == 0) sum += x;
-        }
-        
-        for (auto &q: qu) {
-            int val = q[0];
-            int ind = q[1];
-            
-            if (v[ind] % 2) {
-                v[ind] += val;
-                if (val % 2)
-                    sum += v[ind];
-            } else {
-                if (val % 2) {
-                    sum -= v[ind];
-                    v[ind] += val;
+        for (int &x: v) sum += (x % 2 == 0) ? x : 0;
+        vector<int> ans;
+        for (vector<int> &Q: q) {
+            int index = Q[1];
+            int val = Q[0] + v[index];
+            if (v[index] % 2 == 0) {
+                if (val % 2 == 0) {
+                    sum += val - v[index];
                 } else {
+                    sum -= v[index];
+                }
+            } else {
+                if (val % 2 == 0) {
                     sum += val;
-                    v[ind] += val;
                 }
             }
+            v[index] = val;
             ans.push_back(sum);
         }
         return ans;
