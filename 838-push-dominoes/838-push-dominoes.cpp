8f4a125364ebab50d32adfd9364da147f16b97d9
Viren Kathiriya <42746942+virenkathiriya@users.noreply.github.com>
2022-09-27 08:56:53 +0530

    Time: 98 ms (31.02%), Space: 16.6 MB (66.38%) - LeetHub

--- 838-push-dominoes/838-push-dominoes.cpp
+++ 838-push-dominoes/838-push-dominoes.cpp
@@ -3,12 +3,12 @@ public:
     string pushDominoes(string s) {
         int l = 0, r = 0;
         int N = s.size();
-        vector<pair<int, int>> vp(N, {0, 0});
+        vector<int> vp(N, 0);
         
         int f = 0, len = 0;
         for (int i = 0; i < N; i++) {
             if (s[i] == 'R') {
-                vp[i] = {1, 0};
+                vp[i] = 0;
                 len = 0;
                 f = 1;
             }
@@ -20,14 +20,15 @@ public:
             if (s[i] == '.') {
                 if (f == 1) {
                     len++;
-                    vp[i] = {f, len};
+                    vp[i] = len;
                 }
             }
         }
+        
         f = 0, len = 0;
         for (int i = N - 1; i >= 0; i--) {
             if (s[i] == 'L') {
-                vp[i] = {-1, 0};
+                vp[i] = 0;
                 len = 0;
                 f = -1;
             }
@@ -40,24 +41,24 @@ public:
             if (s[i] == '.') {
                 if (f == -1) {
                     len++;
-                    if (vp[i].first != 0) {
-                        if (vp[i].second == len) {
-                            vp[i] = {0, 0};
-                        } else if (vp[i].second > len) {
-                            vp[i] = {-1, len};
+                    if (vp[i] != 0) {
+                        if (vp[i] == len) {
+                            vp[i] = 0;
+                        } else if (vp[i] > len) {
+                            vp[i] = -len;
                         }
                     } else {
-                        vp[i] = {-1, len};
+                        vp[i] = -1;
                     }
                 }
             }
         }
         
         for (int i = 0; i < N; i++) {
-            int c = vp[i].first;
-            if (c == 0) s[i] = '.';
-            else if (c == 1) s[i] = 'R';
-            else s[i] = 'L';
+            int c = vp[i];
+            if (c > 0 || (c == 0 && s[i] == 'R')) s[i] = 'R';
+            else if (c < 0 || (c == 0 && s[i] == 'L')) s[i] = 'L';
+            else s[i] = '.';
         }
         return s;
     }
