32b7972d4257950bdbe5598abf9c171dfc725ce1
Viren Kathiriya <42746942+virenkathiriya@users.noreply.github.com>
2022-10-01 19:52:36 +0530

    Time: 86 ms (19.25%), Space: 8.4 MB (28.76%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.cpp
+++ 76-minimum-window-substring/76-minimum-window-substring.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
-    bool f(vector<int> &a) {
+    bool f(vector<int> &a, vector<int> &b) {
         bool ok = true;
-        for (int i = 'A'; i <= 'Z'; i++) ok &= (a[i] <= 0);
-        for (int i = 'a'; i <= 'z'; i++) ok &= (a[i] <= 0);
+        for (int i = 'A'; i <= 'Z'; i++) ok &= (a[i] >= b[i]);
+        for (int i = 'a'; i <= 'z'; i++) ok &= (a[i] >= b[i]);
+        
         return ok;
     }
     
@@ -12,20 +13,21 @@ public:
         for (char &c: t) b[c]++;
         
         int i = 0, j = 0, len = INT_MAX, index = -1, N = s.size();
+        vector<int> a(256, 0);
         while (true) {
             if (j >= N) break;
-            while (j < N && !f(b)) {
-                b[s[j]]--;
+            while (j < N && !f(a, b)) {
+                a[s[j]]++;
                 j++;
             }
-            while (i < j && i < N && f(b)) {
+            while (i < j && i < N && f(a, b)) {
                 int curWindowLength = j - i;
                 int ind = i;
                 if (curWindowLength < len) {
                     len = curWindowLength;
                     index = ind;
                 }
-                b[s[i]]++;
+                a[s[i]]--;
                 i++;
             }
         }
