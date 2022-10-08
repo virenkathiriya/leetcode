229caa55b7890eaff9969e2e98c7cc733f532e04
Viren Kathiriya <42746942+virenkathiriya@users.noreply.github.com>
2022-10-08 11:30:37 +0530

    Time: 485 ms (45.64%), Space: 16.3 MB (84.20%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.cpp
+++ 16-3sum-closest/16-3sum-closest.cpp
@@ -3,31 +3,21 @@ public:
     int threeSumClosest(vector<int>& v, int t) {
         sort(v.begin(), v.end());
         int N = v.size();
-        int ans = 1e8;
-        for (int i = 0; i < N; i++) {
-            int a = v[i];
-            int l = i + 1, r = N - 1;
-            while (l < r && l < N && r >= 0) {
-                int b = v[l], c = v[r];
-                if (abs(t - a - b - c) < abs(t - ans)) ans = a + b + c;
-                
-                int l1Diff = INT_MIN, r1Diff = INT_MIN;
-                if (l + 1 < N && l + 1 < r) {
-                    b = v[l + 1], c = v[r];
-                    l1Diff = abs(t - a - b - c);
-                } 
-                if (r - 1 >= 0 && r - 1 > l) {
-                    b = v[l], c = v[r - 1];
-                    r1Diff = abs(t - a - b - c);
-                }
-                // you are getting lesser diff (l + 1 or r - 1) move there
-                if (l1Diff < r1Diff) l++;
-                else r--;
-                
-                if (min(l1Diff, r1Diff) > abs(t - a - b - c)) break;
-            }
+        int ans = v[0] + v[1] + v[2];
+        for (int i = 0; i < N - 2; i++) {
+            int sum = v[i] + twoSumClosest(v, t - v[i], i + 1, N - 1);
+            if (abs(t - sum) < abs(t - ans)) ans = sum;
+        }
+        return ans;
+    }
+    
+    int twoSumClosest(vector<int> &v, int t, int i, int j) {
+        int ans = v[i] + v[j];
+        while (i < j) {
+            int sum = v[i] + v[j];
+            if (abs(t - sum) < abs(t - ans)) ans = sum;
+            (sum > t) ? j--: i++;
         }
         return ans;
     }
 };
- 
