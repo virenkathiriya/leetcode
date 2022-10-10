53972fc07ca0d39df2792e0f48886fc56cf12060
Viren Kathiriya <42746942+virenkathiriya@users.noreply.github.com>
2022-10-10 22:16:12 +0530

    Time: 0 ms (100.00%), Space: 6 MB (14.13%) - LeetHub

--- 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.cpp
+++ 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
     int minOperations(int N) {
-        int ans = 0;
-        for (int i = 1; i <= N; i += 2) {
-            ans += (N - i);
-        }
-        return ans;
+        return N * N / 4;
     }
 };
