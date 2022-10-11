0a155ef781c9602608104a1625304156f9c67c11
Viren Kathiriya <42746942+virenkathiriya@users.noreply.github.com>
2022-10-11 15:31:42 +0530

    Time: 7 ms (97.10%), Space: 10.1 MB (51.40%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& v) {
+        int x = 0, a = 0;
+        for (int &i: v) x ^= i;
+        int bit = 31 - __builtin_clz(x);
+        for(int &i: v) if (i & (1 << bit)) a ^= i;
+        return {a, a ^ x};
+    }
+};
