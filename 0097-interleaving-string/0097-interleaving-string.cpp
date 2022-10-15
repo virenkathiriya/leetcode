3e833b4f16d89b741bbb36d23b1dd54bae867014
Viren Kathiriya <42746942+virenkathiriya@users.noreply.github.com>
2022-10-15 15:03:57 +0530

    Time: 4 ms (78.48%), Space: 6.3 MB (90.88%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,31 +1,23 @@
 class Solution {
 public:
-    int f(int i, int j, string &a, string &b, string &c, vector<vector<int>> &dp, int aN, int bN, int cN) {
-        if (i > aN || j > bN) return false;
-        if (dp[i][j] != -1) return dp[i][j];
-        if (
-            (i + j == cN - 1) 
-            && (
-                    (i < aN && a[i] == c[i + j]) 
-                    || 
-                    (j < bN && b[j] == c[i + j])
-            )
-        ) return true; 
-        bool ok = false;
-        if (i < aN && a[i] == c[i + j]) {
-            ok |= f(i + 1, j, a, b, c, dp, aN, bN, cN);
-        } 
-        if (j < bN && b[j] == c[i + j]) {
-            ok |= f(i, j + 1, a, b, c, dp, aN, bN, cN);
-        }
-        return dp[i][j] = ok;
-    }
-    
-    bool isInterleave(string s1, string s2, string s3) {
-        int aN = s1.size(), bN = s2.size(), cN = s3.size();
+    bool isInterleave(string a, string b, string c) {
+        int aN = a.size(), bN = b.size(), cN = c.size();
         if (aN == 0 && bN == 0 && cN == 0) return true;
         if (aN + bN != cN) return false;
-        vector<vector<int>> dp(aN + 1, vector<int> (bN + 1, -1));
-        return f(0, 0, s1, s2, s3, dp, aN, bN, cN);
+        vector<bool> dp(bN + 1, 0);
+        
+        for (int i = 0; i <= aN; i++) {
+            for (int j = 0; j <= bN; j++) {
+                if (i == 0 && j == 0) dp[j] = true;
+                else if (i == 0)
+                    dp[j] = dp[j - 1] && b[j - 1] == c[i + j - 1];
+                else if (j == 0)
+                    dp[j] = dp[j] && a[i - 1] == c[i + j - 1];
+                else {
+                    dp[j] = (dp[j] && a[i - 1] == c[i + j - 1]) || (dp[j - 1] && b[j - 1] == c[i + j - 1]);
+                }
+            }
+        }
+        return dp[bN];
     }
 };
