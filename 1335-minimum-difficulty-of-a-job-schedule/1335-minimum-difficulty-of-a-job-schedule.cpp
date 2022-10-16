a22c66c49be22093611ff6a2987f8001de9b7ecb
Viren Kathiriya <42746942+virenkathiriya@users.noreply.github.com>
2022-10-16 12:42:59 +0530

    Time: 105 ms (43.21%), Space: 7.9 MB (30.04%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
@@ -1,31 +1,20 @@
 class Solution {
 public:
-    vector<int> seg;
-    
-    void b(int i, vector<int> &v, int l, int r) {
-        if (l == r) { seg[i] = v[l]; return; }
-        int m = l + (r - l) / 2;
-        b(2 * i + 1, v, l, m), b(2 * i + 2, v, m + 1, r);
-        seg[i] = max(seg[2 * i + 1], seg[2 * i + 2]);
-    }
-    
-    int q(int ind, int i, int j, int l, int r) {
-        if (l <= i and r >= j) return seg[ind];
-        if (l > j or r < i) return INT_MIN;
-        int m = i + (j - i) / 2;
-        return max(q(2 * ind + 1, i, m, l, r), q(2 * ind + 2, m + 1, j, l, r));
-    }
-    
     int f(int i, int d, int N, vector<int>&v, vector<vector<int>> &dp) {
+        if (d == 0 && i == N) return 0;
+        if (d == 0 or i == N) return INT_MAX;
         if (dp[i][d] != INT_MAX) return dp[i][d];
         
-        if (d == 0) return dp[i][d] = q(0, 0, N - 1, i, N - 1);
-        
         int res = INT_MAX;
-        for (int j = i + 1; j < N; j++) {
-            int rem = f(j, d - 1, N, v, dp);
-            if (rem == INT_MAX) continue;
-            res = min(res, q(0, 0, N - 1, i, j - 1) + rem);
+        int _max = INT_MIN;
+        for (int j = i; j < N; j++) {
+            _max = max(_max, v[j]);
+            
+            int rem = f(j + 1, d - 1, N, v, dp);
+            if (rem == INT_MAX) {
+                continue;
+            }
+            res = min(res, _max + rem);
         }
         
         return dp[i][d] = res;
@@ -34,11 +23,7 @@ public:
     int minDifficulty(vector<int>& v, int d) {
         int N = v.size();
         if (N < d) return -1;
-        
-        seg.resize(N * 4 + 1);
-        b(0, v, 0, N - 1);
         vector<vector<int>> dp(N + 1, vector<int> (d + 1, INT_MAX));
-        
-        return f(0, d - 1, N, v, dp);
+        return f(0, d, N, v, dp);
     }
 };
