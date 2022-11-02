c6a444b378c341b7d304dd64d25ba013cc89d56c
Viren Kathiriya <42746942+virenkathiriya@users.noreply.github.com>
2022-11-02 09:04:39 +0530

    Time: 8 ms (9.25%), Space: 7 MB (7.33%) - LeetHub

---
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int minMutation(string start, string end, vector<string>& bank) {
+        queue<string> q;
+        unordered_map<string, bool> vis;
+        unordered_map<string, bool> bankmap;
+        for (string &s: bank) bankmap[s] = true;
+        q.push(start);
+        vis[start] = true;
+        
+        int ans = 0;
+        while (!q.empty()) {
+            int sz = q.size();
+            for (int i = 0; i < sz; i++) {
+                string s = q.front();
+                q.pop();
+                for (int j = 0; j < 8; j++) {
+                    for (char c: "ACGT") {
+                        string next = s;
+                        next[j] = c;
+                        if (next == end && bankmap[next]) return ans + 1;
+                        
+                        if (!vis[next] && bankmap[next]) {
+                            q.push(next);
+                            vis[next] = true;
+                        }
+                    }
+                }
+            }
+            ans++;
+        }
+        return -1;
+    }
+};
