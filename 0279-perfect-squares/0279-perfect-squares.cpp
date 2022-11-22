c81937cc57941498cf29a4d727f4f86d3dc27ca7
Viren Kathiriya <42746942+virenkathiriya@users.noreply.github.com>
2022-11-22 08:32:25 +0530

    Time: 223 ms (73.06%), Space: 9 MB (69.87%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -2,17 +2,14 @@ class Solution {
 public:
     int numSquares(int n) {
         int m = sqrt(n) + 1;
-        vector<int> v(m + 1, 0);
-        for (int i = 0; i <= m; ++i) v[i] = i * i;
-        
         vector<int> dp(n + 1, INT_MAX);
         
         dp[0] = 0;
         
         for (int i = 1; i <= m; ++i) {
             for (int j = 0; j < n + 1; j++) {
-                if (j - v[i] >= 0)
-                dp[j] = min(dp[j], dp[j - v[i]] + 1);
+                if (j - (i * i) >= 0)
+                dp[j] = min(dp[j], dp[j - (i * i)] + 1);
             }
         }
         
